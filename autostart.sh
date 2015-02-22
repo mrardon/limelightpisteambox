ba124f7d57f405e6e92debd14b0520c78cf8f6be
Matthew Rardon <mrardon@ncees.org>
2015-02-21 23:32:02 -0500

    Use some variables

--- autostart.sh
+++ autostart.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#PC Info
+MAC="<ENTER MAC HERE e.g. 12:34:56:78:90:12>"
+HOSTNAME="<ENTER IP OR HOSTNAME OF PC HERE>"
+
 #Start PC
-wakeonlan <ENTER MAC HERE e.g. 12:34:56:78:90:12>
+wakeonlan $MAC
 
 #Wait until steam is reachable
-echo Connecting to the PC ...
-while ! nc -z -w5 <ENTER IP OR HOSTNAME OF PC HERE> 47989 &>/dev/null; do :; done
+echo Connecting to the PC ($HOSTNAME)...
+while ! nc -z -w5 $HOSTNAME 47989 &>/dev/null; do :; done
 
 #Start limelight
-echo PC available, starting limelight
+echo PC available at $HOSTNAME, starting limelight
 cd /home/pi/limelight
-java -jar limelight.jar -mapping xbox.map -60fps stream <ENTER IP OR HOSTNAME OF PC HERE>
+java -jar limelight.jar -mapping xbox.map -60fps stream $HOSTNAME
